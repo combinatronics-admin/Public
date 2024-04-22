8104f2d7a318051392900a4fda2bc22faf2c5949
Combinatronics <140537951+combinatronics-admin@users.noreply.github.com>
2024-04-22 16:04:46 +0200

    Update pricing.js

--- pricing.js
+++ pricing.js
@@ -1,3 +1,8 @@
+var jQueryScript = document.createElement('script');
+jQueryScript.setAttribute('src', 'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js');
+document.head.appendChild(jQueryScript);
+
+
 window.ondragstart=function() {
 	return false
 } 
