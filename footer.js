a03b989bf3bc92d7c19c7664d828de653a76fd12
Combinatronics <140537951+combinatronics-admin@users.noreply.github.com>
2024-07-28 16:23:42 +0200

    Update footer.js

--- footer.js
+++ footer.js
@@ -751,7 +751,7 @@ document.head.appendChild(style);
 
 
 var script = document.createElement('script');
-script.src = 'https://code.jquery.com/jquery-3.6.3.min.js'; // Check https://jquery.com/ for the current version
+script.src = 'https://code.jquery.com/jquery-3.7.1.min.js'; // Check https://jquery.com/ for the current version
 document.getElementsByTagName('head')[0].appendChild(script);
 
 
