aa6e212096312d0ce57013eceaa41cae0d6d0157
Combinatronics <140537951+combinatronics-admin@users.noreply.github.com>
2024-07-28 16:16:39 +0200

    Update footer.js

--- footer.js
+++ footer.js
@@ -748,7 +748,14 @@ const style = document.createElement("style");
 style.textContent = ".title h1{ font-size: 30px;} .social-proof img{float:left;} .social-proof{ white-space: pre-wrap; margin-bottom:60px;} .box { overflow: auto; }";
 document.head.appendChild(style);  
 
-  
+
+
+var script = document.createElement('script');
+script.src = 'https://code.jquery.com/jquery-3.6.3.min.js'; // Check https://jquery.com/ for the current version
+document.getElementsByTagName('head')[0].appendChild(script);
+
+
+
 $('<div />', {
   html: '<div class="social-proof"><img height="40px" src="https://opensearch.org/assets/media/partners/cern/cern_logo.png"><hr><br><h3>CERN</h3><p>Uses Combinatronics CDN to create an addictive incremental game that teaches players the history of high energy particle physics.</p></div>'
 }).insertAfter('.social-proof:last');
